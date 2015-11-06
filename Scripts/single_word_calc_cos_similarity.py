f44b96b5e21d83ed38f456778d7276fa3c2c79b9
Abhinav Jain <abhinavjain241@gmail.com>
2015-11-06 23:38:18 +0530

    Dynamic method added

--- Scripts/single_word_calc_cos_similarity.py
+++ Scripts/single_word_calc_cos_similarity.py
@@ -1,48 +1,46 @@
 from sklearn.metrics.pairwise import cosine_similarity
-# from scipy import spatial
+
+gloveFilePath = "../Data/glove.6B.300d.txt"
+newDataFilePath = "../Data/the_20022005_clusters_N200.txt"
+oldDataFilePath = "../Data/the_19091953_clusters_N200.txt"
 
 hash = {}
 
-with open("../Data/glove.6B.300d.txt") as infile:
+with open(gloveFilePath) as infile:
 	for line in infile:
 		word, dim_string = line.split(' ', 1)
 		dimension = dim_string.split('\n')[0].split(' ')
 		hash[word] = dimension
 
-# m = 0 # number of lines in old
-# n = 0 # number of lines in new
-
 matrix = []
 
-with open("../Data/the_20022005_clusters_N200.txt") as new_data:
-	with open("../Data/the_19091953_clusters_N200.txt") as old_data:
-		for line_in_new in new_data:
-			sl_n = line_in_new.split("\t")
-			sl1_n = sl_n[2].split(", ")
-			wlist_n = []
-			for item in sl1_n:
-				wlist_n.append(item.split("/")[0])		
+with open(newDataFilePath) as new_data:
+	with open(oldDataFilePath) as old_data:
+		for lineInNew in new_data:
+			splitNew = lineInNew.split("\t")
+			splitNewWords = splitNew[2].split(", ")
+			wordListNew = []
+			for item in splitNewWords:
+				wordListNew.append(item.split("/")[0])		
 			# We've got wordlist for new data
 			row = []
-			for line_in_old in old_data:
-				sl_o = line_in_old.split("\t")
-				sl1_o = sl_o[2].split(", ")
-				wlist_o = []
+			for lineInOld in old_data:
+				sl_o = lineInOld.split("\t")
+				splitOldWords = sl_o[2].split(", ")
+				wordListOld = []
 				for item in sl_o:
-					wlist_o.append(item.split("/")[0])
-
+					wordListOld.append(item.split("/")[0])
+				# We've got worlist for old data
 				# Compute Cosine similarity of each sense cluster
 				result = 0 
 				count = 0
-				for word_new in wlist_n:
-					for word_old in wlist_o:
+				for word_new in wordListNew:
+					for word_old in wordListOld:
 						if ((word_new in hash) and (word_old in hash)):
 							count = count + 1
 							result += cosine_similarity(hash[word_new], hash[word_old])
 				result = result / count
 				# End of computation for one sense cluster
 				row.append(result)
-
 			matrix.append(row)
-
 print matrix
